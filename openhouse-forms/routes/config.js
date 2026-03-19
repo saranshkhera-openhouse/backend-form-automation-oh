876c45f93bd09b68a384c8cac1b382fbb3fd5f7a
Sahaj Dureja <durejasahaj@gmail.com>
2026-03-19 12:00:30 +0530

    Update config.js

--- openhouse-forms/routes/config.js
+++ openhouse-forms/routes/config.js
@@ -16,7 +16,7 @@ module.exports=function(pool){
     yesNo:["Yes","No"],
     cityMap:{"Gurgaon":"G","Noida":"N","Ghaziabad":"GH"},
     sourceMap:{"CP":"C","Direct":"D"},
-    assignedByList: ["Abhishek Rathore", "Aman Dixit", "Animesh Singh", "Apurv Nath", "Arti Ahirwar","Deepak Mishra", "Kavita Rawat", "Prateek Shrivastava", "Rahul Sheel", "Rupali Prasad", "Sahil Singh", "Shashank Kumar", "Sushmita Roy", "Vaibhav Dwivedi","Test Sahaj"],
+    assignedByList: ["Abhishek Rathore", "Aman Dixit", "Animesh Singh", "Apurv Nath", "Arti Ahirwar","Deepak Mishra", "Kavita Rawat", "Prateek Shrivastava","Nisha Deewan", "Rahul Sheel", "Rupali Prasad", "Sahil Singh", "Shashank Kumar", "Sushmita Roy", "Vaibhav Dwivedi","Test Sahaj"],
     assignedToList: ["Aman Dixit" , "Animesh Singh","Deepak Mishra","Nishant Singh", "Praveen Kumar", "Rahul Sheel", "Rahul Singh", "Sahil Singh","Vaibhav Dwivedi","Test Sahaj"]
   };
   router.get('/',(_,r)=>r.json({options:OPT}));
