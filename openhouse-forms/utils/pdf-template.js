64ceeb25dee2b98fca7d167a0deac858d9e05c52
Sahaj Dureja <sahaj.dureja@openhouse.in>
2026-04-02 17:24:23 +0530

    pdf token footer edit

--- openhouse-forms/utils/pdf-template.js
+++ openhouse-forms/utils/pdf-template.js
@@ -188,7 +188,7 @@ function generateReceiptHTML(p, mode='deal', baseUrl=''){
   </div><div class="footer">
     <div><div class="footer-brand">Avano Technologies Private Limited</div>
       <div class="footer-cin">CIN: U68200HR2024PTC123116 | VentureX, Unit No. 202 &amp; 202A, Silverton Tower, Sector 50, Golf Course Extension Road, Gurugram 122018</div></div>
-    <div class="footer-note">Token Receipt<br><a href="https://www.openhouse.in" style="color:var(--muted);text-decoration:none">www.openhouse.in</a></div>
+    <div class="footer-note"><br><a href="https://www.openhouse.in" style="color:var(--muted);text-decoration:none">www.openhouse.in</a></div>
   </div>
   </div><div class="page-2-content" style="page-break-before:always">
     <div class="p2-body">
